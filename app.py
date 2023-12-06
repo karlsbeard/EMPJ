2227705cdaa15c786201c92339e30cf3371899a4
chengkai <2557075675@qq.com>
2023-12-06 15:46:54 +0800

    feat: 完成api测试

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+@app.route('/test', methods=['GET', 'POST'])
+def test_api():
+    if request.method == 'GET':
+        # 返回一个简单的消息
+        return jsonify({'message': 'GET 请求成功！', 'data': '这里可以放一些数据'})
+
+        # 如果是POST请求
+    elif request.method == 'POST':
+        # 获取JSON数据
+        data = request.json
+        print(f'getData {data}')
+        # 返回接收到的数据，以及一个确认消息
+        return jsonify({'message': 'POST 请求成功！', 'received_data': data})
+
+        # 其他请求类型处理（可选）
+    else:
+        return jsonify({'message': '不支持的请求类型'})
 
 
 @app.route('/')
---
+++ config.py
@@ -0,0 +1,7 @@
+class DevelopmentConfig:
+    DEBUG = True
+    # 其他开发环境特定配置
+
+class ProductionConfig:
+    DEBUG = False
+    # 其他生产环境特定配置
